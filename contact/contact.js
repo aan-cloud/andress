b4a17467bd260afc40b6cd33b8bfe1c7a07055a3
Farhan <fmuh8402@gmail.com>
2024-06-16 16:56:58 +0800

    fitur update contacts

--- contact/contact.js
+++ contact/contact.js
@@ -1,18 +1,19 @@
 // Data
-const dataContact = {
-    datas: [
-        {
-            firstName: "",
-            lastName: "",
-            phoneNumber: "",
-            emailAddress: "",
-            region: ''
-        }
-    ]
-};
+let dataContact = [
+
+];
+
+const emptyData = {
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    emailAddress: "",
+    region: ""
+}
 
 // input contact info
-const firsName = document.querySelector('#first_name');
+const id = document.querySelector('#id');
+const firstName = document.querySelector('#first_name');
 const lastName = document.querySelector('#last_name');
 const phone = document.querySelector('#phone');
 const email = document.querySelector('#email');
@@ -21,78 +22,116 @@ const region = document.querySelector('#region');
 
 // create table element to add data contact
 const form = document.querySelector('form');
+const buttonUpdate = document.querySelector('#update');
 const overlay = document.querySelector('#overlay');
 const message = document.getElementById('message');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const tbody = document.querySelector('tbody');
+// bikin elemen table dan masing masing column nya di isi dengan innertext yang sesuai pada data input
+// ambil isi input field
+// tambah kan isi input field ke dalam array 
+// buat table row sesuai dengan input field
+function createRow (data) {
+    
     const row = document.createElement('tr');
     row.classList.add('bg-white', 'border-b', 'dark:bg-gray-800', 'dark:border-gray-700', 'hover:bg-gray-50', 'dark:hover:bg-gray-600');
-    tbody.appendChild(row);
 
     const fullName = document.createElement('th');
     fullName.classList.add('px-6', 'py-4', 'font-medium', 'text-gray-900', 'whitespace-nowrap', 'dark:text-white');
+    fullName.innerText = data.firstName + ` ${data.lastName}`;
     row.appendChild(fullName);
 
     const emailAddres = document.createElement('td');
-    emailAddres.classList.add('px-6','py-4');
+    emailAddres.classList.add('px-6', 'py-4');
+    emailAddres.innerText = data.emailAddress;
     row.appendChild(emailAddres);
 
     const phoneNumber = document.createElement('td');
-    phoneNumber.classList.add('px-6','py-4');
+    phoneNumber.classList.add('px-6', 'py-4');
+    phoneNumber.innerText = data.phoneNumber;
     row.appendChild(phoneNumber);
 
     const country = document.createElement('td');
-    country.classList.add('px-6','py-4');
+    country.classList.add('px-6', 'py-4');
+    country.innerText = data.region;
     row.appendChild(country);
 
     const edit = document.createElement('td');
-    edit.classList.add('px-6','py-4');
+    edit.classList.add('px-6', 'py-4');
     row.appendChild(edit);
 
-    const textEdit = document.createElement('a');
-    textEdit.classList.add('font-medium', 'text-blue-600', 'dark:text-blue-500', 'hover:underline');
+    const textEdit = document.createElement('button');
+    textEdit.classList.add('font-medium', 'text-blue-600', 'dark:text-blue-500', 'hover:underline', 'edit');
     textEdit.innerText = 'edit';
+    textEdit.onclick = editContact;
     edit.appendChild(textEdit);
-
-
-    dataContact.datas.forEach((data) => {
-        data.firstName = firsName.value;
-        data.lastName = lastName.value;
-        data.phoneNumber = phone.value;
-        data.emailAddress = email.value;
-        data.region = region.value;
-        fullName.innerText = data.firstName + ` ${data.lastName}`;
-        emailAddres.innerText = data.emailAddress;
-        phoneNumber.innerText = data.phoneNumber;
-        country.innerText = data.region;
-    });
-
+    return row
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target))
+    // pecah pecah function agar bisa di gunakkan kembali
+    // 
+    dataContact.push(data);
+    // sampai sini
+    // ambil data nya pakai get element
+    // buat row dan tambah an row
+    const tbody = document.querySelector('tbody');
+    const row = createRow(data)
+    tbody.appendChild(row)
 
 
     form.classList.add('hidden');
     overlay.classList.add('hidden');
     message.remove();
+    console.log(dataContact)
 });
+
+buttonUpdate.addEventListener('click', (e) => {
+    // ambil data dari form
+    // update data contact
+    // buat row dan ganti row
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(form));
+    // ubah value dari key nya sesuai input data update
+    dataContact[data.id-1] = data;
+    const tbody = document.querySelector('tbody');
+    const oldRow = tbody.getElementsByTagName('tr')[data.id-1];
+    const row = createRow(data);
+    tbody.replaceChild(row, oldRow);
+    form.classList.add('hidden');
+    overlay.classList.add('hidden');
+    message.remove();
+})
 
 
 // membuat element form
 const buttonAdd = document.querySelector('#button-add');
 
-buttonAdd.addEventListener('click', function createDataForm (data) {
-    firsName.value = "";
-    lastName.value = "";
-    phone.value = "";
-    email.value = "";
-    region.value = "";
-    
-    form.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+function openForm(data) {
+    return function () {
+        id.value = data.id;
+        firstName.value = data.firstName;
+        lastName.value = data.lastName;
+        phone.value = data.phoneNumber;
+        email.value = data.emailAddress;
+        region.value = data.region;
 
-    // close button
-    document.getElementById('close-btn').addEventListener('click', () => {
-        form.classList.add('hidden');
-        overlay.classList.add('hidden');
-    });
-});
+        form.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+
+        // close button
+        document.getElementById('close-btn').addEventListener('click', () => {
+            form.classList.add('hidden');
+            overlay.classList.add('hidden');
+        });
+    }
+}
+
+// cara nya adalah dengan membuat form baru ketika event edit
+// dan membuat event submit baru ketika form edit di submit
+// dapat kan elemen nya dan ganti innertext nya sesuai input
+buttonAdd.addEventListener('click',() => openForm({...emptyData,id:dataContact.length+1})());
+
+function editContact(e) {
+    const rowIndex = e.target.parentElement.parentElement.rowIndex;
+    openForm(dataContact[rowIndex-1])();
+}
